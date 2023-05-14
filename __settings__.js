6402d91fc0db571d8f5d651f250adb5c17f4f604
chenhongye <346105613@qq.com>
2023-05-14 10:06:37 +0800

    提交

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1350527.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 71179025, 71179013, 71621669 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/71178988/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/71178981/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/71178985/1/ammo.js', 'preload' : true},
+];
